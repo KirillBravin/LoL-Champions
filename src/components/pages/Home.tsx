a802fcf292fcc59cc864905db0f3673ee70a401b
Phil <kshaadoo1991@gmail.com>
2024-02-15 19:58:38 +0200

    minor changes

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,7 +3,6 @@ import { SkinDiscounts } from "../skinDiscounts/SkinDiscounts";
 export function Home() {
   return (
     <>
-      <h1>Home</h1>
       <SkinDiscounts />
     </>
   );
--- src/components/skinDiscounts/SkinDiscounts.tsx
+++ src/components/skinDiscounts/SkinDiscounts.tsx
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
+
 export function SkinDiscounts() {
   return (
-    <section className="skin-discount">
-      <h2 className="d-flex justify-content-center">Weekly skin discount</h2>
+    <section className="skin-discount h-500">
+      <Container>
+        <h2 className="d-flex justify-content-center pt-5">
+          Weekly skin discount
+        </h2>
+      </Container>
     </section>
   );
 }
