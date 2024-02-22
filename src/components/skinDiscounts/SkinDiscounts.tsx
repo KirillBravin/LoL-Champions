95c2ef9a5dd012782feaee87c2424d60af27ca5d
Phil <kshaadoo1991@gmail.com>
2024-02-22 14:10:31 +0200

    Few files rearrangement

--- src/components/skinDiscounts/SkinDiscounts.tsx
+++ src/components/skinDiscounts/SkinDiscounts.tsx
@@ -4,7 +4,7 @@ import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../../style/skinDiscount.sass";
+import "../skinDiscounts/skinDiscount.sass";
 
 export function SkinDiscounts() {
   const { getChampionLoading } = useLeagueService();
