d63b8b9405cf1a8f23a66b236fca54bafce7f548
Kirill Bravin <kshaadoo1991@gmail.com>
2024-03-10 16:46:41 +0200

    Added basic files, texts, classes

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,16 @@
+import "./title.css";
+
+export function Title() {
+  return (
+    <>
+      <h1 className="title title_wrapper">
+        <div>
+          <span className="title__intro">choose your</span>
+        </div>
+        <div>
+          <strong className="title__main">champion</strong>
+        </div>
+      </h1>
+    </>
+  );
+}
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { SkinDiscounts } from "../skinDiscounts/SkinDiscounts";
 import { ChampSearchForm } from "../champSearchForm/ChampSearchForm";
+import { Title } from "../Title/Title";
 
 export function Home() {
   return (
     <>
       <SkinDiscounts />
+      <Title />
       <ChampSearchForm />
     </>
   );
