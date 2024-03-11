3dc56515ee45a2f9a20ad7e3808f0ad6d947ad6c
Kirill Bravin <kshaadoo1991@gmail.com>
2024-03-11 16:59:34 +0200

    Basic search bar form

--- src/components/champSearchForm/ChampSearchForm.tsx
+++ src/components/champSearchForm/ChampSearchForm.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
 export function ChampSearchForm() {
-  return <h2>Champ form</h2>;
+  const [searchItem, setSearchItem] = useState("");
+
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearchItem(searchTerm);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchItem}
+        onChange={handleInputChange}
+        placeholder="Search"
+      />
+    </div>
+  );
 }
