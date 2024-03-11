da0324b0f3d532abf3d03fcb8f474e2183f72626
Kirill Bravin <kshaadoo1991@gmail.com>
2024-03-11 14:58:23 +0200

    Added animations to title

--- src/components/pages/homePage/Home.tsx
+++ src/components/pages/homePage/Home.tsx
@@ -1,6 +1,7 @@
 import { SkinDiscounts } from "../../skinDiscounts/SkinDiscounts";
 import { ChampSearchForm } from "../../champSearchForm/ChampSearchForm";
 
+import "animate.css";
 import "./home.sass";
 
 export function Home() {
@@ -8,9 +9,13 @@ export function Home() {
     <>
       <SkinDiscounts />
       <h1 className="title">
-        <span className="title__intro">choose your</span>
-        <strong className="title__main">champion</strong>
-        <span className="title__subtitle">
+        <span className="animate__animated title__intro animate__fadeIn">
+          choose your
+        </span>
+        <strong className="animate__animated title__main animate__fadeIn">
+          champion
+        </strong>
+        <span className="animate__animated title__subtitle animate__fadeIn">
           With more than 140 champions, you’ll find the perfect match for your
           playstyle. Master one, or master them all.
         </span>
