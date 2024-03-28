9b8b57b0cb3a76caaaee2729dff1d4e0fd1c756a
Kirill Bravin <kshaadoo1991@gmail.com>
2024-03-28 16:44:34 +0200

    Small bug fixes: search-bar left side hover area and line adjusted

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App/App.tsx";
 import "./style/style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
