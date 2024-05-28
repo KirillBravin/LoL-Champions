4c67c787d4c87ed2b0ad3a50dd9b8d0d4ac9133a
Kirill Bravin <kshaadoo1991@gmail.com>
2024-05-28 16:42:11 +0300

    Added background role icon

--- src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
+++ src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
@@ -2,9 +2,18 @@ import "./championInfoAbilities.scss";
 
 export function ChampionAbilities() {
   return (
-    <>
-      <h1>hi</h1>
-      <h2>world</h2>
-    </>
+    <div className="championAbilities">
+      <div className="championAbilities-style__background">
+        <img
+          className="championAbilities-style__icon"
+          src="https://www.leagueoflegends.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleMage.svg"
+          alt="Mage"
+        />
+      </div>
+      <div className="championAbilities-style__abilities">Abilities</div>
+      <div className="container">
+        <h2 className="championAbilities-style__header">Abilities</h2>
+      </div>
+    </div>
   );
 }
