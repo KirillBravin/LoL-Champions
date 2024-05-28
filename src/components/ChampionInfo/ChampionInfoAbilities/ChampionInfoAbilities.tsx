d79166663d5ef525b98f1827061e58cb086ff6e8
Kirill Bravin <kshaadoo1991@gmail.com>
2024-05-28 18:47:20 +0300

    added ability icons

--- src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
+++ src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
@@ -10,9 +10,43 @@ export function ChampionAbilities() {
           alt="Mage"
         />
       </div>
-      <div className="championAbilities-style__abilities">Abilities</div>
+      <div className="championAbilities-style__abilities-sideText">
+        Abilities
+      </div>
       <div className="container">
-        <h2 className="championAbilities-style__header">Abilities</h2>
+        <div className="championAbilities-style__wrapper">
+          <div className="championAbilities-style__abilities-info">
+            <h2 className="championAbilities-style__header">Abilities</h2>
+            <div className="championAbilities-style__ability-icons-wrapper">
+              <img
+                className="championAbilities-style__ability-icon"
+                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/passive/Ahri_SoulEater2.png"
+                alt=""
+              />
+              <img
+                className="championAbilities-style__ability-icon"
+                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriQ.png"
+                alt=""
+              />
+              <img
+                className="championAbilities-style__ability-icon"
+                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriW.png"
+                alt=""
+              />
+              <img
+                className="championAbilities-style__ability-icon"
+                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriE.png"
+                alt=""
+              />
+              <img
+                className="championAbilities-style__ability-icon"
+                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriR.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="championAbilities-style__abilities-preview"></div>
+        </div>
       </div>
     </div>
   );
