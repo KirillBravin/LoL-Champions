85ac1528a1f911ec9eca86665067faf9c44a6535
Kirill Bravin <kshaadoo1991@gmail.com>
2024-05-31 15:24:06 +0300

    Added active effects to icons and hover effects

--- src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
+++ src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
@@ -1,6 +1,59 @@
+import { useState } from "react";
 import "./championInfoAbilities.scss";
 
+interface Abilities {
+  abilityType: string;
+  name: string;
+}
+
 export function ChampionAbilities() {
+  const [iconIsActive, setIconIsActive] = useState<number | null>(null);
+
+  const iconClicked = (i: number) => {
+    setIconIsActive(i === iconIsActive ? null : i);
+  };
+
+  const abilities: Abilities[] = [
+    { abilityType: "passive", name: "Ahri_SoulEater2" },
+    { abilityType: "spell", name: "AhriQ" },
+    { abilityType: "spell", name: "AhriW" },
+    { abilityType: "spell", name: "AhriE" },
+    { abilityType: "spell", name: "AhriR" },
+  ];
+
+  const getImagePath = (ability: Abilities) => {
+    if (ability.abilityType === "spell") {
+      return "spell";
+    } else return "passive";
+  };
+
+  const abilityIcons = (arg: Abilities[]) => {
+    const items = arg.map((item, index) => {
+      return (
+        <img
+          key={index}
+          onClick={() => {
+            iconClicked(index);
+          }}
+          className={`championAbilities-style__ability-icon ${
+            index === iconIsActive ? "ability-icon_active" : ""
+          }`}
+          src={`https://ddragon.leagueoflegends.com/cdn/14.10.1/img/${getImagePath(
+            item
+          )}/${item.name}.png`}
+          alt=""
+        />
+      );
+    });
+    return (
+      <div className="championAbilities-style__ability-icons-wrapper">
+        {items}
+      </div>
+    );
+  };
+
+  const iconsRender = abilityIcons(abilities);
+
   return (
     <div className="championAbilities">
       <div className="championAbilities-style__background">
@@ -17,33 +70,8 @@ export function ChampionAbilities() {
         <div className="championAbilities-style__wrapper">
           <div className="championAbilities-style__abilities-info">
             <h2 className="championAbilities-style__header">Abilities</h2>
-            <div className="championAbilities-style__ability-icons-wrapper">
-              <img
-                className="championAbilities-style__ability-icon"
-                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/passive/Ahri_SoulEater2.png"
-                alt=""
-              />
-              <img
-                className="championAbilities-style__ability-icon"
-                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriQ.png"
-                alt=""
-              />
-              <img
-                className="championAbilities-style__ability-icon"
-                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriW.png"
-                alt=""
-              />
-              <img
-                className="championAbilities-style__ability-icon"
-                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriE.png"
-                alt=""
-              />
-              <img
-                className="championAbilities-style__ability-icon"
-                src="https://ddragon.leagueoflegends.com/cdn/14.10.1/img/spell/AhriR.png"
-                alt=""
-              />
-            </div>
+            {iconsRender}
+            <div className="championAbilities-style__diving-line"></div>
           </div>
           <div className="championAbilities-style__abilities-preview"></div>
         </div>
