7b245c4da727c662b6247d6e647deefb23207963
Kirill Bravin <kshaadoo1991@gmail.com>
2024-06-02 12:55:47 +0300

    Created component for description of abilities

--- src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
+++ src/components/ChampionInfo/ChampionInfoAbilities/ChampionInfoAbilities.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./championInfoAbilities.scss";
 
+import { ChampionInfoAbilityDescriptions } from "../ChampionInfoAbilityDescriptions/ChampionInfoAbilityDescriptions";
+
 interface Abilities {
   abilityType: string;
   name: string;
@@ -8,9 +10,21 @@ interface Abilities {
 
 export function ChampionAbilities() {
   const [iconIsActive, setIconIsActive] = useState<number | null>(null);
+  const [abilitySelected, setAbilitySelected] = useState<string>("");
 
   const iconClicked = (i: number) => {
     setIconIsActive(i === iconIsActive ? null : i);
+    if (i === 0) {
+      setAbilitySelected("Passive");
+    } else if (i === 1) {
+      setAbilitySelected("SpellQ");
+    } else if (i === 2) {
+      setAbilitySelected("SpellW");
+    } else if (i === 3) {
+      setAbilitySelected("SpellE");
+    } else if (i === 4) {
+      setAbilitySelected("SpellR");
+    }
   };
 
   const abilities: Abilities[] = [
@@ -19,39 +33,6 @@ export function ChampionAbilities() {
     { abilityType: "spell", name: "AhriW" },
     { abilityType: "spell", name: "AhriE" },
     { abilityType: "spell", name: "AhriR" },
-  ];
-
-  const description = [
-    {
-      name: "Essence Theft",
-      type: "Passive",
-      descr:
-        "After killing 9 minions or monsters, Ahri heals.<br>After taking down an enemy champion, Ahri heals for a greater amount.",
-    },
-    {
-      name: "Orb of Deception",
-      type: "Q",
-      descr:
-        "Ahri sends out and pulls back her orb, dealing magic damage on the way out and true damage on the way back.",
-    },
-    {
-      name: "Fox-Fire",
-      type: "W",
-      descrp:
-        "Ahri gains a brief burst of Move Speed and releases three fox-fires, that lock onto and attack nearby enemies.",
-    },
-    {
-      name: "Charm",
-      type: "E",
-      descr:
-        "Ahri blows a kiss that damages and charms an enemy it encounters, instantly stopping movement abilities and causing them to walk harmlessly towards her.",
-    },
-    {
-      name: "Spirit Rush",
-      type: "R",
-      descr:
-        "Ahri dashes forward and fires essence bolts, damaging nearby enemies. Spirit Rush can be cast up to three times before going on cooldown, and gains additional recasts when taking down enemy champions.",
-    },
   ];
 
   const getImagePath = (ability: Abilities) => {
@@ -119,18 +100,7 @@ export function ChampionAbilities() {
             <h2 className="championAbilities-style__header">Abilities</h2>
             {iconsRender}
             <div className="championAbilities-style__diving-line"></div>
-            <div className="championAbilities-style__description-wrapper">
-              <div className="championAbilities-style__description-ability-type">
-                Passive
-              </div>
-              <div className="championAbilities-style__description-ability-name">
-                Essence Theft
-              </div>
-              <div className="championAbilities-style__description-ability">
-                After killing 9 minions or monsters, Ahri heals. After taking
-                down an enemy champion, Ahri heals for a greater amount.
-              </div>
-            </div>
+            <ChampionInfoAbilityDescriptions />
           </div>
           <div className="championAbilities-style__abilities-preview"></div>
         </div>
