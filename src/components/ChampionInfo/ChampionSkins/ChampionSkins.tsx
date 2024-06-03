17a19208dfc799529d2b6f1d83fea4acb58dd859
Kirill Bravin <kshaadoo1991@gmail.com>
2024-06-03 21:06:42 +0300

    Some changes to the slider, changed paths and now clicking on slide changes background

--- src/components/ChampionInfo/ChampionSkins/ChampionSkins.tsx
+++ src/components/ChampionInfo/ChampionSkins/ChampionSkins.tsx
@@ -2,80 +2,83 @@ import "@splidejs/react-splide/css";
 import "./ChampionSkins.scss";
 
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { useState } from "react";
 
 export function ChampionSkins() {
+  const [skinSelected, setSkinSelected] = useState("Ahri_0.jpg");
+
   const ahriSkins = [
     {
       name: "Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_0.jpg",
+      path: "Ahri_0.jpg",
     },
     {
       name: "Dynasty Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_1.jpg",
+      path: "Ahri_1.jpg",
     },
     {
       name: "Midnight Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_2.jpg",
+      path: "Ahri_2.jpg",
     },
     {
       name: "Foxfire Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_3.jpg",
+      path: "Ahri_3.jpg",
     },
     {
       name: "Popstar Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_4.jpg",
+      path: "Ahri_4.jpg",
     },
     {
       name: "Challenger Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_5.jpg",
+      path: "Ahri_5.jpg",
     },
     {
       name: "Academy Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_6.jpg",
+      path: "Ahri_6.jpg",
     },
     {
       name: "Arcade Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_7.jpg",
+      path: "Ahri_7.jpg",
     },
     {
       name: "Star Guardian Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_14.jpg",
+      path: "Ahri_14.jpg",
     },
     {
       name: "K/DA Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_15.jpg",
+      path: "Ahri_15.jpg",
     },
     {
       name: "Prestige K/DA Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_16.jpg",
+      path: "Ahri_16.jpg",
     },
     {
       name: "Elderwood Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_17.jpg",
+      path: "Ahri_17.jpg",
     },
     {
       name: "Spirit Blossom Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_27.jpg",
+      path: "Ahri_27.jpg",
     },
     {
       name: "K/DA ALL OUT Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_28.jpg",
+      path: "Ahri_28.jpg",
     },
     {
       name: "Coven Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_42.jpg",
+      path: "Ahri_42.jpg",
     },
     {
       name: "Prestige K/DA Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_65.jpg",
+      path: "Ahri_65.jpg",
     },
     {
       name: "Arcana Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_66.jpg",
+      path: "Ahri_66.jpg",
     },
     {
       name: "Snow Moon Ahri",
-      path: "https://ddragon.leagueoflegends.com/cdn/img/champion/centered/Ahri_76.jpg",
+      path: "Ahri_76.jpg",
     },
   ];
 
@@ -84,8 +87,11 @@ export function ChampionSkins() {
       return (
         <SplideSlide key={i}>
           <img
+            onClick={() => {
+              setSkinSelected(item.path);
+            }}
             className="championSkins__slider__skin-image"
-            src={item.path}
+            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${item.path}`}
             alt={item.name}
           />
         </SplideSlide>
@@ -101,7 +107,7 @@ export function ChampionSkins() {
         <div className="championSkins__slider-wrapper">
           <img
             className="championSkins__slider-background"
-            src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg"
+            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${skinSelected}`}
             alt="champion skin"
           />
           <div className="championSkins__slider-cover">
@@ -113,10 +119,10 @@ export function ChampionSkins() {
                 height: `47.1rem`,
                 wheel: true,
                 arrows: false,
-                /* pagination: false, */
+                pagination: false,
                 perPage: 3,
                 gap: `10px`,
-                speed: 1000,
+                speed: 2000,
                 easing: "ease",
               }}
             >
